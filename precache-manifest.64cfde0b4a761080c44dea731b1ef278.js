0068dbd35005df4e0eb4e4ffe356d24e7397a023
Kalysh <503292@ukr.net>
2019-10-13 16:18:07 +0300

    Updates

---
+++ precache-manifest.64cfde0b4a761080c44dea731b1ef278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e87b5dbd1703365a04cc9dec83ccb95",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "35ab0552167e86c886c6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6101b924.chunk.css"
+  },
+  {
+    "revision": "6a156e3130dd2742df6d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.8339b503.chunk.js"
+  },
+  {
+    "revision": "35ab0552167e86c886c6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.1ed90307.chunk.js"
+  },
+  {
+    "revision": "716f83227a277155b007",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.75015d9a.js"
+  },
+  {
+    "revision": "0210d67624b81e69857a30fae7ecd065",
+    "url": "/goit-react-hw-03-image-finder/static/media/Moogle.0210d676.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.../precache-manifest.e8219648c5011d141c4619c97cec04de.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.64cfde0b4a761080c44dea731b1ef278.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/.../index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-image-finder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
