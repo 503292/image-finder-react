9346a2a24cc756c96c4f8c1a5ba67e873b83b74a
Kalysh <503292@ukr.net>
2019-12-05 11:05:57 +0200

    REFACT _ path for favicon

--- src/services/photo-api.js
+++ src/services/photo-api.js
@@ -9,5 +9,3 @@ export const getPhotos = (query = '', page = 1) =>
   axios.get(
     `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12${API_KEY}`,
   );
-
-// 13892199-58c163103ab3e33586f8e4337   DENA
