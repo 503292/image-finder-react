aebcdf9448c04a0231cf913563b966747b1d62b0
Kalysh <503292@ukr.net>
2019-12-05 11:38:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.f9c886d526098b627c0b826b67d908d3.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.304adc0730a8274a8d3a1012fe805bc9.js"
 );
 
 self.addEventListener('message', (event) => {
